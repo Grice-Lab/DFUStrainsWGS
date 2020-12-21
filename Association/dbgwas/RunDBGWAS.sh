fa7d670183abe3ca42a2ed56221d7bf429c9d456
Amo <amyecampbell@gmail.com>
2020-12-21 10:45:20 -0500

    running dbgwas on xanthin

---
+++ Association/dbgwas/RunDBGWAS.sh
@@ -0,0 +1,7 @@
+# Amy Campbell
+# December 2020
+# Association testing with DBGWAS 
+
+source /home/acampbe/software/miniconda3/bin/activate DBGWASEnv
+
+/home/acampbe/software/DBGWAS/bin/DBGWAS -strains XanthinMapping -newick /home/acampbe/DFU/data/WGS_2020/Trees/RaxML_NoRefs/RAxML_bestTree.RaxMLTreeNoRefs.newick -output /home/acampbe/DFU/data/WGS_2020/DBGWAS_Output/
