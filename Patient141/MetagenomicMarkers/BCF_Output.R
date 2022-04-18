3e47535ec54452a5c14e4084244b086915a72afe
amyecampbell <amyecampbell@gmail.com>
2022-04-18 14:25:52 -0400

    trying again

--- Patient141/MetagenomicMarkers/BCF_Output.R
+++ Patient141/MetagenomicMarkers/BCF_Output.R
@@ -34,26 +34,46 @@ BCF_files= AllFiles[grepl(AllFiles, pattern=".bcf")]
 
 
 OutputDF = data.frame()
-#colnames(OutputDF) = c("Source", "MarkerID", "TotalUseableDepth", "RefAlleleDepth", "AltAlleleDepth")
 
 for(FilePath in BCF_files){
-
+  
+  # Get info for this file
+  BCFPrefix <- (str_split(filename, pattern="reads_"))[[1]][1]
+  BCFSuffix <- str_remove((str_split(filename, pattern="reads_"))[[1]][2], pattern=".bcf")
+  listparams=str_split(BCFSuffix, pattern="_")
+  dvalue <- as.numeric(listparams[[1]][1])
+  rvalue <-  as.numeric(listparams[[1]][2])
+  lvalue <-  as.numeric(listparams[[1]][3])
+  qvalue <-  as.numeric(listparams[[1]][4])
+  
+  # Get Full path for this file
   FullPath=paste0(FDirectory, FilePath)
+  
+  # Try opening this file
   trystatement = try(read.delim(FullPath, comment.char = "#", header=F), silent=T)
+  
+  # If it's empty other than comment lines, 
+  #colnames(OutputDF) = c("Source", "MarkerID", "TotalUseableDepth", "RefAlleleDepth", "AltAlleleDepth")
+  
   if(inherits(trystatement, "try-error")){
+    
+    marker1 = c("Marker1", 0,0,0)
+    NewRow = append(BCFPrefix,marker1)
+    OutputDF = rbind(OutputDF, NewRow)
+    
+    marker2= c("Marker2", 0,0,0)
+    NewRow = append(BCFPrefix,marker2)
+    OutputDF = rbind(OutputDF, NewRow)
+    
+    marker3=c("Marker3", 0,0,0)
+    NewRow = append(BCFPrefix,marker3)
+    OutputDF = rbind(OutputDF, NewRow)
+    
     print("Empty File, sRy!")} else{
       
       FileObject=read.delim(FullPath,comment.char = "#", header=F)
       colnames(FileObject) = c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO","FORMAT", "V10")
       filename=basename(FilePath)
-      
-      BCFPrefix <- (str_split(filename, pattern="reads_"))[[1]][1]
-      BCFSuffix <- str_remove((str_split(filename, pattern="reads_"))[[1]][2], pattern=".bcf")
-      listparams=str_split(BCFSuffix, pattern="_")
-      dvalue <- as.numeric(listparams[[1]][1])
-      rvalue <-  as.numeric(listparams[[1]][2])
-      lvalue <-  as.numeric(listparams[[1]][3])
-      qvalue <-  as.numeric(listparams[[1]][4])
       
       marker1 =MarkerVals(FileObject, 250, "Marker1")
       NewRow = append(BCFPrefix,marker1)
@@ -68,13 +88,7 @@ for(FilePath in BCF_files){
       OutputDF = rbind(OutputDF, NewRow)
       
       
-    
-      
     }
-  
-  
-  
-  
   
 }
 
