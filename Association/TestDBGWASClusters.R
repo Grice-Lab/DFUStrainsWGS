8e66c8ad641b1aeddd0f47d5f10bf16f75e42128
amyecampbell <amyecampbell@gmail.com>
2022-06-07 16:05:58 -0400

    updated inflation plots for the KDE based clustering

--- Association/TestDBGWASClusters.R
+++ Association/TestDBGWASClusters.R
@@ -4,76 +4,222 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 
-# data is in /Users/amycampbell/Documents/TestingXanthinDBGWASSubsets
-# where, on the LPC:
-# Subset219Unitigs.tsv = /home/acampbe/DFU/data/WGS_2020/DBGWAS_Test_219_Xanthin/textualOutput/all_comps_nodes_info.tsv
-# Subset104Unitigs.tsv = /home/acampbe/DFU/data/WGS_2020/DBGWAS_Test_104_Xanthin/textualOutput/all_comps_nodes_info.tsv
-# Subset98Unitigs.tsv = /home/acampbe/DFU/data/WGS_2020/DBGWAS_Test_98_Xanthin/textualOutput/all_comps_nodes_info.tsv
-# BUGWASPatterns219.txt = /home/acampbe/DFU/data/WGS_2020/DBGWAS_Test_219_Xanthin/step2/patterns.txt
-# BUGWASPatterns104.txt = /home/acampbe/DFU/data/WGS_2020/DBGWAS_Test_104_Xanthin/step2/patterns.txt
-# BUGWASPatterns98.txt = /home/acampbe/DFU/data/WGS_2020/DBGWAS_Test_98_Xanthin/step2/patterns.txt
+# Update 6/7/22 
+# To include all phenotypes, each subset by KDE on that phenostype (zero-one normalized)
+
+setwd("~/Desktop/GriceLabGit/DFUStrainsWGS/")
+
+# Inflation in Xanthin
+#######################
+patternsFullXanthin = read_delim("data/dbgwas2022/XanthinZeroOne/bugwasPatternsXanthinZeroOne.txt", " ",col_names=F)
+colnames(patternsFullXanthin) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
+
+patternsSubsetXanthin = read_delim("data/dbgwas2022/XanthinSubsetZeroOne/bugwasPatternsXanthinKDEsubset.txt", " ",col_names=F)
+colnames(patternsSubsetXanthin) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
+
+pvectFullXanthin =  patternsFullXanthin$`p-value`
+pvectSubsetXanthin =  patternsSubsetXanthin$`p-value`
 
 
-setwd("/Users/amycampbell/Documents/TestingXanthinDBGWASSubsets")
+qqobservedFullXanthin= -log10(sort(pvectFullXanthin,decreasing=FALSE))
+qqobservedSubsetXanthin = -log10(sort(pvectSubsetXanthin,decreasing=FALSE))
+
+qqexpectFullXanthin = -log10( ppoints(length(pvectFullXanthin) ))
+qqexpectSubsetXanthin = -log10( ppoints(length(pvectSubsetXanthin) ))
 
 
-# Inflation in each
-###################
+dataframeFullXanthin= data.frame(expect=qqexpectFullXanthin, observe=qqobservedFullXanthin)
+dataframeSubsetXanthin = data.frame(expect=qqexpectSubsetXanthin, observe=qqobservedSubsetXanthin)
 
-# Full set of isolates Amelia's using
-bugWASpatterns219 = read_delim("BUGWASPatterns219.txt", " ", col_names=T)
+plotobjXanthin = ggplot(data=dataframeFullXanthin, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for staphyloxanthin DBGWAS p-values")  + theme_classic() +geom_point(data=dataframeSubsetXanthin, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
 
-# K means clusters + patients + 50 SNP cutoff 
-bugWASpatterns104 = read_delim("BUGWASPatterns104.txt", " ", col_names=T)
+Siderophore 
+#############################
+patternsFullSiderophore = read_delim("data/dbgwas2022/SiderophoreZeroOne/bugwasPatternsSiderophoreZeroOne.txt", " ",col_names=F)
+colnames(patternsFullSiderophore) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
 
-# K means clusters + patients + MLST CCs 
-bugWASpatterns98 =read_delim("BUGWASPatterns98.txt", " ", col_names=T)
+patternsSubsetSiderophore = read_delim("data/dbgwas2022/SiderophoreSubsetZeroOne/bugwasPatternsSiderophoreSubset.txt", " ",col_names=F)
+colnames(patternsSubsetSiderophore) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
 
-pvect219 =  bugWASpatterns219$`p-value`
-pvect104 =  bugWASpatterns104$`p-value`
-pvect98 = bugWASpatterns98$`p-value`
-
-qqobserved219 = -log10(sort(pvect219,decreasing=FALSE))
-qqobserved104 = -log10(sort(pvect104,decreasing=FALSE))
-qqobserved98 = -log10(sort(pvect98,decreasing=FALSE))
-
-qqexpect219 = -log10( ppoints(length(pvect219) ))
-qqexpect104 = -log10( ppoints(length(pvect104) ))
-qqexpect98 = -log10( ppoints(length(pvect98) ))
+pvectFullSiderophore =  patternsFullSiderophore$`p-value`
+pvectSubsetSiderophore =  patternsSubsetSiderophore$`p-value`
 
 
-dataframe219 = data.frame(expect=qqexpect219, observe=qqobserved219)
-dataframe104 = data.frame(expect=qqexpect104, observe=qqobserved104)
-dataframe98 = data.frame(expect=qqexpect98, observe=qqobserved98)
+qqobservedFullSiderophore= -log10(sort(pvectFullSiderophore,decreasing=FALSE))
+qqobservedSubsetSiderophore = -log10(sort(pvectSubsetSiderophore,decreasing=FALSE))
 
-plotobj = ggplot(data=dataframe219, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for DBGWAS p-values")  + theme_classic()#+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
-plotobj= plotobj + geom_point(data=dataframe104, aes(x=expect, y=observe), color="blue", alpha=.5) + geom_point(data=dataframe98, aes(x=expect, y=observe), color="green", alpha=.5) +
-annotate(geom="text",color="green", label="98-isolate subset", x=1, y=7, size=8) + annotate(geom="text",color="blue", label="104-isolate subset", x=1, y=7.5, size=8) + annotate(geom="text",color="black", label="219-isolate set", x=1, y=6.5, size=8)
+qqexpectFullSiderophore = -log10( ppoints(length(pvectFullSiderophore) ))
+qqexpectSubsetSiderophore = -log10( ppoints(length(pvectSubsetSiderophore) ))
 
 
+dataframeFullSiderophore= data.frame(expect=qqexpectFullSiderophore, observe=qqobservedFullSiderophore)
+dataframeSubsetSiderophore = data.frame(expect=qqexpectSubsetSiderophore, observe=qqobservedSubsetSiderophore)
 
-results219 = read_tsv("Subset219Unitigs.tsv")
-results104 = read_tsv("Subset104Unitigs.tsv")
+plotobjsiderophore = ggplot(data=dataframeFullSiderophore, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for Siderophore DBGWAS")  + theme_classic() +geom_point(data=dataframeSubsetSiderophore, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
 
-results219_top20 = results219 %>% select(`p-value`, `q-Value`,Sequence) %>% arrange(`q-Value`,`p-value` )
-results219_top20 = results219_top20[1:20, ]
 
-# Selected top 47 bc there are two top q-values, when you sort by q then p to include all the ones sharing the p (so probably equally distributed)
-results104_top20 = results104 %>% select(`p-value`, `q-Value`,Sequence) %>% arrange(`q-Value`, `p-value`)
-results104_top44 = results104_top20[1:44, ]
+# Staphylokinase (sak-positive only)
+####################################
+patternsFullKinase = read_delim("data/dbgwas2022/staphylokinaseZeroOne/bugwasPatterns_KinaseZeroOne.txt", " ",col_names=F)
+colnames(patternsFullKinase) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
 
-IntersectingUnitigs = intersect(results219_top20$Sequence,results104_top47$Sequence)
-View(results104 %>% filter(Sequence %in% IntersectingUnitigs))
+patternsSubsetKinase = read_delim("data/dbgwas2022/staphylokinaseSubsetZeroOne/bugwasPatterns_KinaseSubset.txt", " ",col_names=F)
+colnames(patternsSubsetKinase) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
 
-ggsave(plotobj, file="/Users/amycampbell/Desktop/GriceLabGit/DFUStrainsWGS/Plots_Output/InflationPlotThreeClusteringTypes.png")
+pvectFullKinase =  patternsFullKinase$`p-value`
+pvectSubsetKinase =  patternsSubsetKinase$`p-value`
 
-# a7185595174bd26cbba955f405c06990_1798_arcC
-# "bc50a8e1ddf06f071ff651359644e735_2682_group_1096~~~a7185595174bd26cbba955f405c06990_1344_extdb:pgaptmp_001653"  "Select seq ref|WP_000781347.1|	superantigen-like protein SSL5 [Staphylococcus aureus]"
-# "a7185595174bd26cbba955f405c06990_1937_extdb:pgaptmp_002241~~~00f10a58fa7304043b40a4fcde135512_2886_extdb:pgaptmp_002971" "Select seq tpg|HBC4173420.1|	TPA: gluconate permease [Staphylococcus aureus] Select seq ref|WP_031897099.1|	gluconate:H+ symporter [Staphylococcus aureus]  which I htink is gntP https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1797221/
-# "a7185595174bd26cbba955f405c06990_270_extdb:pgaptmp_000486" shikimate kinase [Staphylococcus aureus]
-# And two NAs
 
-# 
+qqobservedFullKinase= -log10(sort(pvectFullKinase,decreasing=FALSE))
+qqobservedSubsetKinase = -log10(sort(pvectSubsetKinase,decreasing=FALSE))
 
-#NAs:
- # TATTTTCACAAAATACTATAATGAGGATAGTAAATAGAGAGGAG
+qqexpectFullKinase = -log10( ppoints(length(pvectFullKinase) ))
+qqexpectSubsetKinase = -log10( ppoints(length(pvectSubsetKinase) ))
+
+
+dataframeFullKinase= data.frame(expect=qqexpectFullKinase, observe=qqobservedFullKinase)
+dataframeSubsetKinase = data.frame(expect=qqexpectSubsetKinase, observe=qqobservedSubsetKinase)
+
+plotobjKinase = ggplot(data=dataframeFullKinase, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for Staphylokinase DBGWAS(sak-positive isolates)")  + theme_classic() +geom_point(data=dataframeSubsetKinase, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
+plotobjKinase
+
+
+
+# Biofilm
+####################################
+patternsFullBiofilm = read_delim("data/dbgwas2022/BiofilmZeroOne/bugwasPatternsBiofilmZeroOne.txt", " ",col_names=F)
+colnames(patternsFullBiofilm) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
+
+patternsSubsetBiofilm = read_delim("data/dbgwas2022/BiofilmSubsetZeroOne/bugwasPatternsBiofilmSubset.txt", " ",col_names=F)
+colnames(patternsSubsetBiofilm) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
+
+pvectFullBiofilm =  patternsFullBiofilm$`p-value`
+pvectSubsetBiofilm =  patternsSubsetBiofilm$`p-value`
+
+
+qqobservedFullBiofilm= -log10(sort(pvectFullBiofilm,decreasing=FALSE))
+qqobservedSubsetBiofilm = -log10(sort(pvectSubsetBiofilm,decreasing=FALSE))
+
+qqexpectFullBiofilm = -log10( ppoints(length(pvectFullBiofilm) ))
+qqexpectSubsetBiofilm = -log10( ppoints(length(pvectSubsetBiofilm) ))
+
+dataframeFullBiofilm= data.frame(expect=qqexpectFullBiofilm, observe=qqobservedFullBiofilm)
+dataframeSubsetBiofilm = data.frame(expect=qqexpectSubsetBiofilm, observe=qqobservedSubsetBiofilm)
+
+plotobjBiofilm = ggplot(data=dataframeFullBiofilm, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for Biofilm DBGWAS")  + theme_classic() +geom_point(data=dataframeSubsetBiofilm, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
+
+
+
+ggsave(gridExtra::grid.arrange(plotobjBiofilm, plotobjXanthin, plotobjKinase, plotobjsiderophore), file="data/dbgwas2022/InflationKDEclusters.pdf")
+
+original = gridExtra::grid.arrange(plotobjBiofilm, plotobjXanthin, plotobjKinase, plotobjsiderophore)
+
+
+# With the ohter patterns 
+##############################
+patternsFullXanthin = read_delim("data/dbgwas2022/XanthinZeroOne/step2/patterns.txt", " ",col_names=T)
+
+patternsSubsetXanthin = read_delim("data/dbgwas2022/XanthinSubsetZeroOne/step2/patterns.txt", " ",col_names=T)
+
+pvectFullXanthin =  patternsFullXanthin$`p-value`
+pvectSubsetXanthin =  patternsSubsetXanthin$`p-value`
+
+
+qqobservedFullXanthin= -log10(sort(pvectFullXanthin,decreasing=FALSE))
+qqobservedSubsetXanthin = -log10(sort(pvectSubsetXanthin,decreasing=FALSE))
+
+qqexpectFullXanthin = -log10( ppoints(length(pvectFullXanthin) ))
+qqexpectSubsetXanthin = -log10( ppoints(length(pvectSubsetXanthin) ))
+
+
+dataframeFullXanthin= data.frame(expect=qqexpectFullXanthin, observe=qqobservedFullXanthin)
+dataframeSubsetXanthin = data.frame(expect=qqexpectSubsetXanthin, observe=qqobservedSubsetXanthin)
+
+plotobjXanthin = ggplot(data=dataframeFullXanthin, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for staphyloxanthin DBGWAS p-values")  + theme_classic() +geom_point(data=dataframeSubsetXanthin, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
+
+#Siderophore 
+#############################
+patternsFullSiderophore = read_delim("data/dbgwas2022/SiderophoreZeroOne/step2/patterns.txt", " ",col_names=T)
+
+patternsSubsetSiderophore = read_delim("data/dbgwas2022/SiderophoreSubsetZeroOne/step2/patterns.txt", " ",col_names=T)
+
+pvectFullSiderophore =  patternsFullSiderophore$`p-value`
+pvectSubsetSiderophore =  patternsSubsetSiderophore$`p-value`
+
+
+qqobservedFullSiderophore= -log10(sort(pvectFullSiderophore,decreasing=FALSE))
+qqobservedSubsetSiderophore = -log10(sort(pvectSubsetSiderophore,decreasing=FALSE))
+
+qqexpectFullSiderophore = -log10( ppoints(length(pvectFullSiderophore) ))
+qqexpectSubsetSiderophore = -log10( ppoints(length(pvectSubsetSiderophore) ))
+
+
+dataframeFullSiderophore= data.frame(expect=qqexpectFullSiderophore, observe=qqobservedFullSiderophore)
+dataframeSubsetSiderophore = data.frame(expect=qqexpectSubsetSiderophore, observe=qqobservedSubsetSiderophore)
+
+plotobjsiderophore = ggplot(data=dataframeFullSiderophore, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for Siderophore DBGWAS")  + theme_classic() +geom_point(data=dataframeSubsetSiderophore, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
+
+
+# Staphylokinase (sak-positive only)
+####################################
+patternsFullKinase = read_delim("data/dbgwas2022/staphylokinaseZeroOne/step2/patterns.txt", " ",col_names=T)
+
+patternsSubsetKinase = read_delim("data/dbgwas2022/staphylokinaseSubsetZeroOne/step2/patterns.txt", " ",col_names=T)
+
+pvectFullKinase =  patternsFullKinase$`p-value`
+pvectSubsetKinase =  patternsSubsetKinase$`p-value`
+
+
+qqobservedFullKinase= -log10(sort(pvectFullKinase,decreasing=FALSE))
+qqobservedSubsetKinase = -log10(sort(pvectSubsetKinase,decreasing=FALSE))
+
+qqexpectFullKinase = -log10( ppoints(length(pvectFullKinase) ))
+qqexpectSubsetKinase = -log10( ppoints(length(pvectSubsetKinase) ))
+
+
+dataframeFullKinase= data.frame(expect=qqexpectFullKinase, observe=qqobservedFullKinase)
+dataframeSubsetKinase = data.frame(expect=qqexpectSubsetKinase, observe=qqobservedSubsetKinase)
+
+plotobjKinase = ggplot(data=dataframeFullKinase, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for Staphylokinase DBGWAS(sak-positive isolates)")  + theme_classic() +geom_point(data=dataframeSubsetKinase, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
+plotobjKinase
+
+
+
+# Biofilm
+####################################
+patternsFullBiofilm = read_delim("data/dbgwas2022/BiofilmZeroOne/step2/patterns.txt", " ",col_names=T)
+#colnames(patternsFullBiofilm) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
+
+patternsSubsetBiofilm = read_delim("data/dbgwas2022/BiofilmSubsetZeroOne/step2/patterns.txt", " ",col_names=T)
+colnames(patternsSubsetBiofilm) =c("pattern", "p-value", "q-value", "weight", "wald_statistic")
+
+pvectFullBiofilm =  patternsFullBiofilm$`p-value`
+pvectSubsetBiofilm =  patternsSubsetBiofilm$`p-value`
+
+
+qqobservedFullBiofilm= -log10(sort(pvectFullBiofilm,decreasing=FALSE))
+qqobservedSubsetBiofilm = -log10(sort(pvectSubsetBiofilm,decreasing=FALSE))
+
+qqexpectFullBiofilm = -log10( ppoints(length(pvectFullBiofilm) ))
+qqexpectSubsetBiofilm = -log10( ppoints(length(pvectSubsetBiofilm) ))
+
+dataframeFullBiofilm= data.frame(expect=qqexpectFullBiofilm, observe=qqobservedFullBiofilm)
+dataframeSubsetBiofilm = data.frame(expect=qqexpectSubsetBiofilm, observe=qqobservedSubsetBiofilm)
+
+plotobjBiofilm = ggplot(data=dataframeFullBiofilm, aes(x=expect, y=observe)) + geom_point() +  geom_abline(slope=1, color="red") + xlim(0, 8) + ylim(0,8)  + xlab("-log10(expected p-values)") + ylab("-log10(observed p-values)") +ggtitle("QQ Plot for Biofilm DBGWAS")  + theme_classic() +geom_point(data=dataframeSubsetBiofilm, aes(x=expect, y=observe), color="blue", alpha=.5) #+ geom_point(data=dataframe_uncorrected, aes(x=expect, y=observe), color="black") + geom_abline(slope=1)
+
+new = gridExtra::grid.arrange(plotobjBiofilm, plotobjXanthin, plotobjKinase, plotobjsiderophore)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
