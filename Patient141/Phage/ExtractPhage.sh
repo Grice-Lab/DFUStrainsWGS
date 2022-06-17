637dd7e9afee994d91f0e172332d775ac1cd5b47
amyecampbell <amyecampbell@gmail.com>
2022-06-17 12:29:45 -0400

    extracting phage sequence from dorn925 for annotation

---
+++ Patient141/Phage/ExtractPhage.sh
@@ -0,0 +1,6 @@
+# Extract stretches of DORN925 corresponding to the phage insertion (mw2 phage)
+
+source /home/acampbe/software/miniconda3/bin/activate prokenv
+
+python3 ExtractPhage925.py  /home/acampbe/DFU/data/WGS_2020/FinalSetStaphIsolates/DORN925_Final.fasta /home/acampbe/DFU/data/WGS_2020/ONP/Patient141/DORN925Phage.fasta
+
---
+++ Patient141/Phage/Extract_Phage.py
@@ -0,0 +1,26 @@
+# Amy Campbell
+# Extracting phage sequences from DORN925 for Patric annotation
+
+from Bio import SeqIO
+import sys
+
+# python3 /home/acampbe/DFU/data/WGS_2020/FinalSetStaphIsolates/DORN925_Final.f$
+
+# e.g. /home/acampbe/DFU/data/WGS_2020/FinalSetStaphIsolates/DORN925_Final.fasta
+DORN925Path = sys.argv[1]
+
+# e.g. /home/acampbe/DFU/data/WGS_2020/ONP/Patient141/DORN925Phage.fasta
+outputPath = sys.argv[2]
+
+contigs = list(SeqIO.parse(DORN925Path, "fasta"))
+contig1 = str(contigs[0].seq)
+
+phageend = contig1[2789823:2806769]
+phagebeginning=contig1[1:25619]
+
+otpt = open(outputPath, "w")
+otpt.write(">DORN925phage1\n")
+otpt.write(phageend)
+otpt.write(">DORN925phage2\n")
+otpt.write(phagebeginning)
+otpt.close()
