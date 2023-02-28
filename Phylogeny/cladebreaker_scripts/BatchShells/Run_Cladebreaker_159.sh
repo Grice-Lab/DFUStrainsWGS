fc17c70eecf613962ecc1907a675256b641fb4ef
amyecampbell <amyecampbell@gmail.com>
2023-02-28 14:35:53 -0500

    updated

--- Phylogeny/cladebreaker_scripts/BatchShells/Run_Cladebreaker_159.sh
+++ Phylogeny/cladebreaker_scripts/BatchShells/Run_Cladebreaker_159.sh
@@ -6,7 +6,7 @@ source ~/mambaforge/bin/activate ~/mambaforge/envs/cladebreaker2
 mkdir -p /home/acampbe/DFU/data/WGS_2020/cladebreaker/cladebreaker_patient_159
 
 cladebreaker \
---input /Users/amycampbell/Desktop/GriceLabGit/DFUStrainsWGS/Phylogeny/cladebreaker_scripts/InputCSVs/patient_159_input.csv \
+--input /home/acampbe/DFU/data/WGS_2020/cladebreaker/InputCSVs/patient_159_input.csv \
 --outdir /home/acampbe/DFU/data/WGS_2020/cladebreaker/cladebreaker_patient_159 \
 --coverage 100 --db /home/acampbe/DownloadedDatabases/WhatsGNU_Sau_Ortholog/Sau_Ortholog_10350.pickle \
 --o --topgenomes_count 5 --force -profile conda -with-conda true \
