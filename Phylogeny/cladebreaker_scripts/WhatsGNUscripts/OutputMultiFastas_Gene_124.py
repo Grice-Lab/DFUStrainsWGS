e96ee1b11fa47920347b7e20ce71efa600625cd1
amyecampbell <amyecampbell@gmail.com>
2023-04-08 15:30:34 -0400

    special script for our 3 clone wound

---
+++ Phylogeny/cladebreaker_scripts/WhatsGNUscripts/OutputMultiFastas_Gene_124.py
@@ -0,0 +1,163 @@
+# Amy Campbell
+# March 2023
+# Preparing to run Mafft on the selected 'differentiating genes'
+# Making a reference version of the gene (<whatever the first DORN it encounters is>_genename.fasta)
+# Then making a multifasta of everyone else's version of the gene in the format, for example of Patient176:
+# Patient176_metI.fasta:
+# > DORN1646_metI
+# ACTGAGCT....
+# > GCA_000555645.1_metI
+# ACTGAGCT....
+
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+import pandas
+import os
+import sys
+
+# Takes in:
+############
+
+# - Path to the FFNs for input (arg 2, should be folder containing only the .ffns you wanna use of the format <genomename>.ffn)
+    # /home/acampbe/DFU/data/WGS_2020/cladebreaker/cladebreaker_patient_176/FFNs
+# - Path to the roary CSV path (arg 3)
+    # /home/acampbe/DFU/data/WGS_2020/cladebreaker/cladebreaker_patient_176/roary_alignment/results/gene_presence_absence.csv
+# - Path to unique genes list output by UniqueWithinGenome.py for CC9 (arg 4)
+    # /home/acampbe/DFU/data/WGS_2020/cladebreaker/cladebreaker_patient_176/Patient176/Patient176_unique.txt
+# - Path to the unique genes list output by UniqueWithinGenome.py for CC8 (arg 5)
+# - Path to output scripts for mafft/snp-sites (arg 6)
+# - Reference genome (arg 7)
+
+# /home/acampbe/DFUStrainsWGS/Phylogeny/cladebreaker_scripts/WhatsGNUscripts/
+
+
+
+# puts out "GeneFastas" folder containing fastas with each gene's sequence in each genome
+# as well as one randomly chosen DORN reference genome's
+# as well as a script called MafftAlignGenes.sh
+#########################################################################################
+
+argsinput = sys.argv
+if len(argsinput) <7:
+    print("Not enough input files actually")
+    exit()
+else:
+    FFNpath = argsinput[1]
+    roaryCSVpath = argsinput[2]
+    UniqueGenesList1 = argsinput[3]
+    UniqueGenesList2 = argsinput[4]
+    OutputScriptDir = argsinput[5]
+    ReferenceGenome = argsinput[6]
+
+
+# Read in the reference genome's ffn (nt sequences for each )
+
+
+IncludedGenomes = (os.listdir(FFNpath))
+
+IncludedDORNs = list(filter(lambda i: 'DORN' in i, IncludedGenomes))
+
+IncludedGenomeNames = list(map(lambda x: x.replace(".ffn", ""),IncludedGenomes))
+
+ReferenceFFN = str(ReferenceGenome) + ".ffn"
+print(ReferenceFFN)
+ReferenceName=ReferenceFFN.replace(".ffn", "")
+ReferencePath=os.path.join(FFNpath, ReferenceFFN)
+
+# Make a big ugly dictionary to pull from
+AllDicts = dict()
+for G in IncludedGenomes:
+    path_fasta=os.path.join(FFNpath, G)
+    GenomeString=G.replace(".ffn", "")
+    AllDicts[GenomeString] = SeqIO.to_dict(SeqIO.parse(path_fasta, "fasta"))
+
+# Read in the Unique Genes' names
+ListOrthologs1 = open(UniqueGenesList1, "r").read().split('\n')
+ListOrthologs1.remove('')
+ListOrthologs2 = open(UniqueGenesList2, "r").read().split('\n')
+ListOrthologs2.remove('')
+
+ListOrthologs=set( ListOrthologs1 + ListOrthologs2 )
+print(ListOrthologs)
+ 
+# Read in the gene presence absence csv from roary as a pandas df
+OrthDF = pandas.read_csv(roaryCSVpath)
+OrthDF = OrthDF[OrthDF['Gene'].isin(ListOrthologs)]
+#SeqIO.write(sequences,handle,"fasta")
+
+# make an output directory
+OutputKey = os.path.dirname(UniqueGenesList1)
+OutputKeyString=os.path.basename(OutputKey)
+
+print(OutputScriptDir)
+OutputScriptMafft=os.path.join(OutputScriptDir, OutputKeyString + "_MafftAlignGenes.sh")
+OutputScriptSNPSites=os.path.join(OutputScriptDir, OutputKeyString + "_SNPsites.sh")
+
+
+OutputDir = os.path.join(OutputKey, "GeneFastas")
+OutputMAFFT = os.path.join(OutputDir, "MAFFT")
+os.mkdir(OutputDir)
+os.mkdir(OutputMAFFT)
+
+
+ReferenceOutputPathPrefix = os.path.join(OutputDir, str(os.path.basename(OutputKey)) + ReferenceName )
+MultiOutputPathPrefix = os.path.join(OutputDir, str(os.path.basename(OutputKey))  )
+
+
+
+FinalListOrths = []
+for o in ListOrthologs:
+    rowItem = (OrthDF[OrthDF.Gene==str(o)])
+
+    ReferenceProtein = str(rowItem[ReferenceName].item())
+    if ReferenceProtein=='nan':
+        print(str(o) + " is missing from reference genome " + str(ReferenceName) + "! Removing from Orthologs List.")
+    else:
+        dictionary_item = (AllDicts[ReferenceName][ReferenceProtein]) # [str(ReferenceProtein)].seq )
+        with open(str(ReferenceOutputPathPrefix) + "_" +  str(o) + "_" + ReferenceName + ".fasta", "w") as refotpt:
+            dictionary_item.id = ReferenceName
+            SeqIO.write(dictionary_item,refotpt, "fasta")
+
+        with open(str(MultiOutputPathPrefix) + "_" + str(o) + ".fasta", "w") as multioutput:
+            for g in IncludedGenomeNames:
+
+                ProteinID = str(rowItem[g].item())
+                # If any one genome is *missing* this gene (weird but possible I guess), remove it from teh list and say so.
+                if ProteinID == 'nan':
+                    print(str(o) + " is missing from " + str(g) + "! Removing from Orthologs List.")
+                    KeepOrth=False
+                else:
+                    KeepOrth=True
+                    dictionary_item = (AllDicts[g][ProteinID])
+                    dictionary_item.id = g
+                    SeqIO.write(dictionary_item, multioutput, "fasta")
+        if KeepOrth == True:
+            FinalListOrths.append(o)
+ListOrths = open(str(MultiOutputPathPrefix + "_FinalOrthologs.txt"), "w")
+
+
+
+for F in FinalListOrths:
+    ListOrths.write(str(F) + "\n")
+
+ListOrths.close()
+
+with open(OutputScriptMafft, "w") as scriptoutput:
+    scriptoutput.write("#!bin/bash\n")
+    scriptoutput.write("# Mafft Alignment on Genes \n")
+    scriptoutput.write("################\n")
+    for f in FinalListOrths:
+        outputstr = "mafft --localpair --addfragments " +  str(MultiOutputPathPrefix) + "_" + str(f) + ".fasta " + str(ReferenceOutputPathPrefix) + "_" +  str(f) + "_" + ReferenceName + ".fasta > " + os.path.join(OutputMAFFT, "Mafft_") + str(f) + "\n"
+        scriptoutput.write(outputstr)
+
+
+with open(OutputScriptSNPSites, "w") as scriptoutput:
+    scriptoutput.write("#!bin/bash\n")
+    scriptoutput.write("# Calls snp-sites on Mafft alignment of genes \n")
+    scriptoutput.write("###############################################\n")
+    for f in FinalListOrths:
+        MafftOut= os.path.join(OutputMAFFT, "Mafft_") + str(f)
+        SNPsitesout=MafftOut.replace("Mafft_", "SNPs_")
+        outputstr = "snp-sites -v -o " + SNPsitesout + " " +MafftOut + "\n"
+        #outputstr = "mafft --localpair --addfragments " +  str(MultiOutputPathPrefix) + "_" + str(f) + ".fasta " + str(ReferenceOutputPathPrefix) + "_" +  str(f) + "_" + ReferenceName + ".fasta > " + os.path.join(OutputMAFFT, "Mafft_") + str(f) + "\n"
+        scriptoutput.write(outputstr)
